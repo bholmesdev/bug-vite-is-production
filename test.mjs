7353976175bcde3990132b7f48383cbd595be491
bholmesdev <hey@bholmes.dev>
2022-07-12 17:16:08 -0400

    first commit

---
+++ test.mjs
@@ -0,0 +1,20 @@
+import * as vite from 'vite'
+
+;(async () => {
+  await vite.build({
+    root: 'src',
+  })
+  const server = await vite.createServer({
+    root: 'src',
+    mode: 'development',
+    plugins: [{
+      name: 'is-production-flipped-on-plugin',
+      enforce: 'pre',
+      configResolved(resolvedConfig) {
+        // should log "true",
+        // even explicitly setting "mode" just above!
+        console.log(resolvedConfig.isProduction)
+      }
+    }]
+  })
+})()
